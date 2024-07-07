c80b1454e2f233e9243a0c3a2faf1513fef26ed4
Pavel Toshinski <pavelce2000@hotmail.com>
2024-07-07 19:32:06 +0200

    added the 4 methods for the recipes

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const {
   deleteStudent,
   updateStudent
 } = require("./handlers/students");
+const {
+  getRecipes,
+  createRecipe,
+  updateRecipe,
+  deleteRecipe
+} = require("./handlers/recipes");
 
 const app = express();
 app.use(express.json());
@@ -13,5 +19,10 @@ app.get("/students", getStudents);
 app.post("/students", createStudent);
 app.put("/students/:index", updateStudent); // added put method
 app.delete("/students/:index", deleteStudent);
+
+app.get("/recipes", getRecipes);
+app.post("/recipes", createRecipe);
+app.put("/recipes/:index", updateRecipe);
+app.delete("/recipes/:index", deleteRecipe);
 
 app.listen(3000, () => console.log("Server running at port 3000!"));
